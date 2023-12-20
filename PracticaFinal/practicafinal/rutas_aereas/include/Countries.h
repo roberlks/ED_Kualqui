935bc6506f0fd343a153b68744707f7da04ffa58
irinaa0021 <uwu.git.infomates@gmail.com>
2023-12-20 21:43:55 +0100

    Class Countries done en teoria

--- PracticaFinal/practicafinal/rutas_aereas/include/Countries.h
+++ PracticaFinal/practicafinal/rutas_aereas/include/Countries.h
@@ -25,9 +25,10 @@ public:
     class iterator{
     private:
         std::set<Country>::iterator c;
+        iterator(std::set<Country>::iterator orig) : c(orig){}
 
     public:
-        iterator(){};
+        iterator(){}
 
         iterator(const iterator &other){
             c = other.c;
@@ -39,19 +40,25 @@ public:
         }
 
         iterator& operator++(){
-            return c++;
+            ++c;
+            return *this;
         }
 
-        iterator& operator++(int){
-            return ++c;
+        iterator operator++(int){
+            iterator aux = *this;
+            ++(*this);
+            return aux;
         }
 
         iterator& operator--(){
-            return c--;
+            --c;
+            return *this;
         }
 
-        iterator& operator--(int){
-            return --c;
+        iterator operator--(int){
+            iterator aux = *this;
+            --(*this);
+            return aux;
         }
 
         bool operator==(const const_iterator& other) const{
@@ -61,7 +68,7 @@ public:
             return !(*this == other);
         }
 
-        Country& operator*(){
+        const Country& operator*(){
             return *c;
         }
 
@@ -72,23 +79,51 @@ public:
     class const_iterator{
     private:
         std::set<Country>::const_iterator c;
+        const_iterator(std::set<Country>::const_iterator orig) : c(orig){}
 
     public:
-        const_iterator();
-        const_iterator(iterator it);
-        const_iterator(const const_iterator &other);
-        const_iterator& operator=(const const_iterator &other);
+        const_iterator(){}
+        const_iterator(const iterator it){
+            c = it.c;
+        }
+        const_iterator(const const_iterator &other){
+            c = other.c;
+        }
+        const_iterator& operator=(const const_iterator &other){
+            c = other.c;
+            return *this;
+        }
 
-        const_iterator& operator++();
-        const_iterator& operator++(int);
+        const_iterator& operator++(){
+            ++c;
+            return *this;
+        }
+        const_iterator operator++(int){
+            const_iterator aux = *this;
+            --(*this);
+            return aux;
+        }
 
-        const_iterator& operator--();
-        const_iterator& operator--(int);
+        const_iterator& operator--(){
+            --c;
+            return *this;
+        }
+        const_iterator operator--(int){
+            const_iterator aux = *this;
+            --(*this);
+            return aux;
+        }
 
-        bool operator==(const const_iterator& other) const;
-        bool operator!=(const const_iterator& other) const;
+        bool operator==(const const_iterator& other) const{
+            return this->c == other.c;
+        }
+        bool operator!=(const const_iterator& other) const{
+            return !(*this == other);
+        }
 
-        const Country& operator*() const;
+        const Country& operator*() const{
+            return *c;
+        }
 
         friend class Countries;
     };
